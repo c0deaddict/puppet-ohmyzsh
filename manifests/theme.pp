29bda4e8ef3332a43312f792fc1081bae5837fa3
Jos van Bakel <jos@codeaddict.org>
2015-07-26 20:09:48 +0200

    Add codeaddict.zsh-theme and install when used

--- manifests/theme.pp
+++ manifests/theme.pp
@@ -2,13 +2,24 @@ define ohmyzsh::theme(
   $theme = 'clean',
   $user = $name
 ) {
-  if $user == 'root' { $home = '/root' } else { $home = "${ohmyzsh::params::home}/${user}" }
-  if $user {
-    file_line { "${user}-${theme}-install":
-      path    => "${home}/.zshrc",
-      line    => "ZSH_THEME=\"${theme}\"",
-      match   => '^ZSH_THEME',
-      require => Ohmyzsh::Install[$user]
+  if $user == 'root' {
+    $home = '/root'
+  } else {
+    $home = "${ohmyzsh::params::home}/${user}"
+  }
+
+  if $theme == 'codeaddict' {
+    file { "$home/.oh-my-zsh/custom/themes/codeaddict.zsh-theme":
+      ensure => present,
+      owner => $user,
+      source => 'puppet:///ohmyzsh/codeaddict.zsh-theme',
     }
+  }
+
+  file_line { "${user}-${theme}-install":
+    path    => "${home}/.zshrc",
+    line    => "ZSH_THEME=\"${theme}\"",
+    match   => '^ZSH_THEME',
+    require => Ohmyzsh::Install[$user],
   }
 }
